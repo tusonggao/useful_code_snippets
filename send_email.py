d04af63cd935baa36ac59354a10c52fc4a20a68f
tusonggao <tusonggao@163.com>
2017-05-23 17:42:02 +0800

    third commit

---
+++ send_email.py
@@ -0,0 +1,27 @@
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((
+          Header(name, 'utf-8').encode(),  
+          addr))
+
+from_addr = 'tusonggao@163.com'
+password = '******'
+to_addr = '360426145@qq.com'
+smtp_server = 'smtp.163.com'
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()

54815cf31e6e3f0125a93a7f2ffc05cd8ca0b702
tusonggao <tusonggao@163.com>
2019-05-07 09:55:13 +0800

    update file

--- send_email.py
+++ send_email.py
@@ -1,27 +1,26 @@
+import smtplib
 from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-import smtplib
 
-def _format_addr(s):
-    name, addr = parseaddr(s)
-    return formataddr((
-          Header(name, 'utf-8').encode(),  
-          addr))
 
-from_addr = 'tusonggao@163.com'
-password = 'tusg070185'
-to_addr = '360426145@qq.com'
-smtp_server = 'smtp.163.com'
+def send_email(from_addr, to_addr, password, subject, body_text):
+    msg = MIMEText(body_text,'html','utf-8')
+    msg['From'] = u'<%s>' % from_addr
+    msg['To'] = u'<%s>' % to_addr
+    msg['Subject'] = subject
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
-msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+    # smtp = smtplib.SMTP_SSL('smtp.163.com', 465)   #for 163.com邮箱
+    smtp = smtplib.SMTP_SSL('smtp.qq.com', 465)  #for qq.com邮箱
+    smtp.set_debuglevel(1)
+    # smtp.ehlo("smtp.163.com")   #for 163.com邮箱
+    smtp.ehlo('smtp.qq.com')  #for qq.com邮箱
+    smtp.login(from_addr, password)
+    smtp.sendmail(from_addr, [to_addr], msg.as_string())
 
-server = smtplib.SMTP(smtp_server, 25)
-server.set_debuglevel(1)
-server.login(from_addr, password)
-server.sendmail(from_addr, [to_addr], msg.as_string())
-server.quit()
+
+body_text = '自动发送的邮件正文'
+
+# send_email("tusonggao@163.com", "360426145@qq.com", "pwd****", "55555 from qq", body_text)
+send_email('360426145@qq.com', 'tusonggao@163.com', 'pwd****', '55555 from qq', body_text)

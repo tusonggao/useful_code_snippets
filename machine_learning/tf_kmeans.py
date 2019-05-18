4c4349519de1f3e5697b4becbe600c5939748d1c
tusonggao <tusonggao@163.com>
2019-05-18 11:46:38 +0800

    add files

--- machine_learning/tf_kmeans.py
+++ machine_learning/tf_kmeans.py
@@ -9,12 +9,13 @@ from sklearn.datasets.samples_generator import make_blobs, make_circles
 
 DATA_TYPES = 'blobs'
 
+N = 200
 if (DATA_TYPES=='circle'):
     K = 2
 else:
     K = 4
 
-MAX_ITERS = 1000
+MAX_ITERS = 100
 start_t = time.time()
 
 centers = [(-2, -2), (-2, 1.5), (1.5, -2), (2, 1.5)]
@@ -42,3 +43,49 @@ centroids = tf.Variable(tf.slice(points.initialized_value(), [0, 0], [K,2]))
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 sess.run(centroids)
+
+print('get here!')
+
+rep_centroids = tf.reshape(tf.tile(centroids, [N, 1]), [N, K, 2])
+rep_points = tf.reshape(tf.tile(points, [1, K]), [N, K,2])
+sum_squares = tf.reduce_sum(tf.square(rep_points - rep_centroids), reduction_indices=2)
+best_centroids = tf.argmin(sum_squares, 1)
+did_assignments_change = tf.reduce_any(tf.not_equal(best_centroids, cluster_assignments))
+
+def bucket_mean(data, bucket_ids, num_buckets):
+    total = tf.unsorted_segment_sum(data, bucket_ids, num_buckets)
+    count = tf.unsorted_segment_sum(tf.ones_like(data), bucket_ids, num_buckets)
+    return total/count
+
+means = bucket_mean(points, best_centroids, K)
+with tf.control_dependencies([did_assignments_change]):
+    do_updates = tf.group()
+    centroids.assign(means)
+    cluster_assignments.assign(best_centroids)
+
+changed = True
+iters = 0
+fig, ax = plt.subplots()
+if DATA_TYPES=='blobs':
+    colourindexes = [2, 1, 4, 3]
+else:
+    colourindexes = [2, 1]
+
+while changed and iters < MAX_ITERS:
+    print('iters is ', iters)
+    fig, ax = plt.subplots()
+    iters += 1
+    [changed, _] = sess.run([did_assignments_change, do_updates])
+    [centers, assignments] = sess.run([centroids, cluster_assignments])
+    ax.scatter(sess.run(points).transpose()[0], sess.run(points).transpose()[1], marker='o', s=200,
+               c=assignments, cmap=plt.cm.coolwarm)
+    ax.scatter(centers[:, 0], centers[:, 1], marker='^', s=550, c=colourindexes, cmap=plt.cm.plasma)
+    ax.set_title('Iteration ' + str(iters))
+    plt.savefig('./pics/kmeans' + str(iters))
+
+ax.scatter(sess.run(points).transpose()[0], sess.run(points).transpose()[1], marker='o', s=200,
+           c=assignments, cmap=plt.cm.coolwarm)
+plt.show()
+end_t = time.time()
+
+print('total cost time: ', end_t-start_t)

168a6f36cc940ab7b953d6aa66e1dbe230e8916f
tusonggao <tusonggao@163.com>
2019-05-19 19:49:18 +0800

    update files

--- machine_learning/tf_logistic_regression.py
+++ machine_learning/tf_logistic_regression.py
@@ -7,12 +7,17 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+def sigmoid(z):
+    return 1 / (1 + np.exp( - z))
+
 def create_mock_data():
+    # np.random.seed(42)
     W = np.array([1.1, 2.2, -3.3, 4.4])
     b = 0.77
     X = np.random.rand(1000, 4)
     y = np.dot(X, W) + b
-    y = np.where(y>0.5, 0, 1)
+    y = np.where(y>3.3, 0, 1)
+    print('X.shape is ', X.shape, y.shape, y.sum())
     return X, y
 
 def batcher(X_data, y_data, batch_size=-1, random_seed=None):
@@ -46,38 +51,52 @@ def logistic_regression_tf(X_train, y_train, X_test, y_test, n_epochs=100, batch
     y = tf.placeholder(dtype=tf.float32, shape=(None, 1), name="y")
     W = tf.Variable(tf.random_uniform([X_train.shape[1], 1], -1.0, 1.0), name='weights')
     b = tf.Variable(0, dtype=tf.float32, name='bias')
-    theta = tf.Variable(tf.random_uniform([X_train.shape[1], 1], -1.0, 1.0), name="theta")
-    y_pred = tf.matmul(X, theta, name="predictions")
+    # y_pred = tf.add(tf.matmul(X, W) + b, name="predictions")
+    y_pred = tf.sigmoid(tf.add(tf.matmul(X, W), b), name='predictions')
 
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
     # optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
     loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=y_pred)
     training_op = optimizer.minimize(loss)
 
+    loss_history, accuracy_history = [], []
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        mse_val = sess.run(mse, feed_dict={X: X_test, y: y_test})
-        print("first MSE =", mse_val)
         for epoch in range(n_epochs):
             # print('epoch is ', epoch)
             for X_batch, y_batch in batcher(X_train, y_train, batch_size):
                 sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
-            if epoch % 10 == 0:
-                mse_val = sess.run(mse, feed_dict={X: X_test, y: y_test})
-                print("Epoch", epoch, "MSE =", mse_val)
-        best_theta = theta.eval()
-        print('best_theta is ', best_theta)
-        mse_val = sess.run(mse, feed_dict={X: X_test, y: y_test})
-        print("final MSE =", mse_val)
+
+        y_class_pred = np.where(y_pred.eval()>0.5, 1, 0)
+        correct_prediction = tf.equal(y_class_pred, y_test)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        # Storing Loss and Accuracy to the history
+        loss_history.append(sum(sum(c)))
+        accuracy_history.append(accuracy.eval({X: x, Y: y}) * 100)
+
+        # if epoch % 10 == 0:
+        #     mse_val = sess.run(mse, feed_dict={X: X_test, y: y_test})
+        #     print("Epoch", epoch, "MSE =", mse_val)
+        best_W = W.eval()
+        print('best_W is ', W)
+
+        plt.plot(list(range(epochs)), loss_history)
+        plt.plot(list(range(epochs)), accuracy_history)
+        plt.xlabel('Epochs')
+        plt.ylabel('Cost')
+        plt.title('Decrease in Cost with Epochs')
+        plt.show()
 
 
 if __name__=='__main__':
-    housing = fetch_california_housing()
-    X_data = housing.data
-    y_data = housing.target
-
-    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
-    print('X_train.shape is', X_train.shape, 'y_train.shape is', y_train.shape)
-    print('X_test.shape is', X_test.shape, 'y_test.shape is', y_test.shape)
-    linear_regression_tf(X_train, y_train, X_test, y_test)
+    X_data, y_data = create_mock_data()
+    # housing = fetch_california_housing()
+    # X_data = housing.data
+    # y_data = housing.target
+    #
+    # X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
+    # print('X_train.shape is', X_train.shape, 'y_train.shape is', y_train.shape)
+    # print('X_test.shape is', X_test.shape, 'y_test.shape is', y_test.shape)
+    # logistic_regression_tf(X_train, y_train, X_test, y_test)

e04099037ebc81d9507633ed5ccdc336947ffa29
tusonggao <tusonggao@163.com>
2019-05-29 13:50:32 +0800

    update

--- flask_api_client.py
+++ flask_api_client.py
@@ -1,6 +1,6 @@
 # 使用post方式时，数据放在data或者body中，不能放在url中，放在url中将被忽略。
 
-# select count(*) from jkbd.quanzi_user_force_recentbuy;   8940  原来14041  明天应该是最新的
+# select count(*) from jkbd.quanzi_user_force_recentbuy;   8940  原来14041  明天应该是最新的   --8539
 
 
 import time
@@ -14,19 +14,19 @@ start_t = time.time()
 #------------------------------------------------------------------------------
 
 # c = http.client.HTTPConnection('172.21.57.127', 7777)   # 14041
-# c = http.client.HTTPConnection('users-cycles-recommend.dev.jianke.com', 80)  # 14041
-#
-#
-# headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-# # c.request('GET', '/get_recentbuy_force', '{}', headers)  # 8940
-# # c.request('GET', '/get_recommend/A9343E00-EDC9-40EA-91FA-D41BE979F970', '{}', headers)
-# c.request('GET', '/get_recommend/88B88334-B470-4E7B-8BAD-F99FFFBEA284', '{}', headers) # for ceshi  88B88334-B470-4E7B-8BAD-F99FFFBEA284
-# s = c.getresponse().read().strip()
-#
-# print('s is ', s)
-# ddd = json.loads(s)
-#
-# print('ddd is ', len(ddd))
+c = http.client.HTTPConnection('users-cycles-recommend.dev.jianke.com', 80)  # 14041
+
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+# c.request('GET', '/get_recentbuy_force', '{}', headers)  # 8940
+# c.request('GET', '/get_recommend/A9343E00-EDC9-40EA-91FA-D41BE979F970', '{}', headers)
+c.request('GET', '/get_recommend/88B88334-B470-4E7B-8BAD-F99FFFBEA284', '{}', headers) # for ceshi  88B88334-B470-4E7B-8BAD-F99FFFBEA284
+s = c.getresponse().read().strip()
+
+print('s is ', s)
+ddd = json.loads(s)
+
+print('ddd is ', len(ddd))
 
 #------------------------------------------------------------------------------
 
@@ -38,12 +38,12 @@ response = requests.get('https://fe-acgi.jianke.com/v2/products/475907?manual=tr
 # print(response.cookies)      # 打印cookie信息
 # print(response.text)         # 以文本形式打印网页源码
 
-ddd = json.loads(response.text)
-
-if 'success' in ddd and ddd['success']==False:
-    print('success is false')
-else:
-    print('ddd[data] is ', ddd['mainData'])
+# ddd = json.loads(response.text)
+#
+# if 'success' in ddd and ddd['success']==False:
+#     print('success is false')
+# else:
+#     print('ddd[data] is ', ddd['mainData'])
 
 
 

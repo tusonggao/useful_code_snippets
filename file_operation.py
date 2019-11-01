3a8af618a7a64589b3ee3326b87b4cdcba32a0a5
tusonggao <tusonggao@163.com>
2019-11-01 17:33:58 +0800

    update file

--- file_operation.py
+++ file_operation.py
@@ -1,4 +1,18 @@
 import os
 
 
+def get_all_files(dir_name):   # 递归得到文件夹下的所有文件
+    all_files_lst = []
+    def get_all_files_worker(path):
+        allfilelist = os.listdir(path)
+        for file in allfilelist:
+            filepath = os.path.join(path, file)
+            #判断是不是文件夹
+            if os.path.isdir(filepath):
+                get_all_files_worker(filepath)
+            else:
+                all_files_lst.append(filepath)
+    get_all_files_worker(dir_name)
+    return all_files_lst
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 得到当前文件的的父目录

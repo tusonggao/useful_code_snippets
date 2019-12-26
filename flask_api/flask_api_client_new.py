755f1913621f0f46d37f292b9bae0668ddd5b172
tusonggao <tusonggao@163.com>
2019-12-26 16:20:46 +0800

    update

--- flask_api/flask_api_client_new.py
+++ flask_api/flask_api_client_new.py
@@ -48,21 +48,39 @@ params = {
                                    # {"outcome":["269069","3772","4450","9647","11929"],"algorithm":"NEW_ITEM_BASED"}  last_read_data_datetime: 2019-08-29 19:17:38
                                    # {"outcome":["269069","1555","7159","3772","4450"],"algorithm":"NEW_ITEM_BASED"}  {"outcome":["20190901155952"]
                                    # {"outcome":["769392","208850","11807","182769","551409"],"algorithm":"NEW_ITEM_BASED"}
+                                   # {"outcome":["551409","11807","551455","853642","208850"],"algorithm":"NEW_ITEM_BASED"}  ["20191209152753"]
+
          # 551997 769392#11807#208850#551409#182769#199516#11808#251645#551455#53416#226388#597721#179016#227350#6414#  2019.9.3  14:33  from table
          # 551997 769392#208850#11807#182769#551409#11808#199516#551455#53416#251645#226388#227350#6414#597721#849093#853642# 2019.9.4  21:33  from table
          #  'product_code': 759860,  # {"outcome":["9058","536009","279770","6465","8757"],"algorithm":"NEW_ITEM_BASED"}  错误的
                                    # {"outcome":["823976","511379","511399","511378","602474"],"algorithm":"NEW_ITEM_BASED"} {"outcome":["20190903211934"], 2019-09-04 9:15查询 开始恢复正常
+                                   # {"outcome":["511399","823976","511379","602474","511378"],"algorithm":"NEW_ITEM_BASED"}   ["20191209152753"]
+                                   # {"outcome":["511379","823976","511399","602474","511378"],"algorithm":"NEW_ITEM_BASED"}   ["20191210152532"]
          # 759860数据表查询 823976#511379#511399#511378#602474#602559#602465#511356#562081#602476#333594#511363#489861    2019.9.3 14:33  from table
 
-          'product_code': 11692,  # {"outcome":["710515","160028","323977","269069","764808"],"algorithm":"NEW_ITEM_BASED"}
-                                  # {"outcome":["710515","269069","764808","675","207422"],"algorithm":"NEW_ITEM_BASED"}
-                                  # {"outcome":["710515","269069","764808","207422","767968"],"algorithm":"NEW_ITEM_BASED"}
-                                  # {"outcome":["710515","269069","675","207422","805082"],"algorithm":"NEW_ITEM_BASED"}
+         'product_code': 11692,  # {"outcome":["710515","160028","323977","269069","764808"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","764808","675","207422"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","764808","207422","767968"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","675","207422","805082"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","207422","764808","675"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","764808","207422","11929"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","764808","207422","11929"],"algorithm":"NEW_ITEM_BASED"} "20191107162527"
+                                 # {"outcome":["710515","269069","764808","207422","11929"],"algorithm":"NEW_ITEM_BASED"}
+                                 # {"outcome":["710515","269069","764808","767968","207422"],"algorithm":"NEW_ITEM_BASED"} "20191111161912"
+                                 # {"outcome":["710515","269069","767968","207422","675"],"algorithm":"NEW_ITEM_BASED"}  20191120152311
+                                 # {"outcome":["269069","767968","207422","764808","805082"],"algorithm":"NEW_ITEM_BASED"}  ["20191202152925"]
+                                 # {"outcome":["269069","805082","207422","767968","764808"],"algorithm":"NEW_ITEM_BASED"}  ["20191205152330"]
+                                 # {"outcome":["269069","207422","764808","767968","675"],"algorithm":"NEW_ITEM_BASED"}  ["20191208152242"]
+                                 # {"outcome":["767968","764808","269069","675","207422"],"algorithm":"NEW_ITEM_BASED"}  ["20191209152753"]
+                                 # {"outcome":["767968","269069","764808","675","207422"],"algorithm":"NEW_ITEM_BASED"}  ["20191210152532"]
+                                 # {"outcome":["767968","269069","675","764808","805082"],"algorithm":"NEW_ITEM_BASED"}  ["20191215153006"]
+                                 # {"outcome":["767968","269069","675","764808","805082"],"algorithm":"NEW_ITEM_BASED"}  ["20191215153006"]
+                                 # {"outcome":["675","767968","269069","764808","805082"],"algorithm":"NEW_ITEM_BASED"}  ["20191223154112"]
+                                 # {"outcome":["269069","767968","675","838349","805082"],"algorithm":"NEW_ITEM_BASED"}  ["20191225152357"]
 
-          # 'product_code': 999999999,
+          # 'product_code': 999999999,    # {"outcome":["269069","805082","207422","767968","764808"],"algorithm":"NEW_ITEM_BASED"}
           # 'user_id': '0011tttyy88oo99uu'
 }
-
 
 # params = {'product_code': '286002',
 #           'user_id': '0011tttyy88oo99333'}  # item-based
@@ -104,6 +122,7 @@ headers = {
 response = requests.get(host_name + '/item/recommend', headers=headers, params=params)
 
 print(response.text)
+print('time cost: ', time.time()-start_t)
 
 # algo_lst = []
 # for i in range(500):

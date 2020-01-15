ebf5079e57b2f0234fdb5d103f8417e195d54e94
tusonggao <tusonggao@163.com>
2020-01-15 19:58:59 +0800

    update

--- file_operation.py
+++ file_operation.py
@@ -1,10 +1,14 @@
 import os
 
 
-import os
-for dirname, _, filenames in os.walk('/kaggle/input/'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+def get_all_files_dirs(root_dir_name):  # 得到文件夹下所有文件名路径和目录路径（会递归到子目录）
+    filenames_lst, dirnames_lst = [], []
+    for dirpath, dirnames, filenames in os.walk(root_dir_name):
+        for filename in filenames:
+            filenames_lst.append(os.path.join(dirpath, filename))
+        for dirname in dirnames:
+            dirnames_lst.append(os.path.join(dirpath, dirname))
+    return filenames_lst, dirnames_lst
 
 
 def get_all_files(dir_name):   # 递归得到文件夹下的所有文件
@@ -21,6 +25,10 @@ def get_all_files(dir_name):   # 递归得到文件夹下的所有文件
     get_all_files_worker(dir_name)
     return all_files_lst
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 得到当前文件的的父目录
 
-dst_file_name = os.path.split(file_name_with_directory)[1]  # 得到当前文件的文件名，例如输入./dogs-vs-cats/test1/test1/10.jpg 得到10.jpg
+filenames, _ = get_all_files_dirs('./flask_api/')
+print('filenames is ', filenames)
+
+# dst_file_name = os.path.split(file_name_with_directory)[1]  # 得到当前文件的文件名，例如输入./dogs-vs-cats/test1/test1/10.jpg 得到10.jpg
